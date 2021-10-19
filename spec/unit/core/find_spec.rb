60ecc8501f42d970aaf43edca33bb8dc7615d264
Ryan <rmammina@gmail.com>
2021-10-18 22:51:36 -0500

    Add functional test cases for find command. Add unit testing on typed regex types

--- spec/unit/core/find_spec.rb
+++ spec/unit/core/find_spec.rb
@@ -5,5 +5,8 @@ require 'spec_helper'
 RSpec.describe Kanrisuru::Core::Find do
   it 'responds to find fields' do
     expect(Kanrisuru::Core::Find::FilePath.new).to respond_to(:path)
+    expect(Kanrisuru::Core::Find::REGEX_TYPES).to(
+      eq(['emacs', 'posix-awk', 'posix-basic', 'posix-egrep', 'posix-extended'])
+    )
   end
 end
