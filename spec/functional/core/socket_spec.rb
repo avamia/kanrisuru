a002536e28d0993e5f29d06dfbd5cc5a185a219e
Ryan <rmammina@gmail.com>
2021-10-31 00:18:03 -0500

    add functional test cases for ss

---
+++ spec/functional/core/socket_spec.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+StubNetwork.stub!
+
+RSpec.describe Kanrisuru::Core::Socket do
+  let(:host) do
+    Kanrisuru::Remote::Host.new(
+      host: 'localhost',
+      username: 'ubuntu',
+      keys: ['id_rsa']
+    )
+  end
+
+  it 'prepares ss command' do
+    expect_command(host.ss, 'ss -a -m')
+
+    expect_command(host.ss(
+        numeric: true,
+        tcp: true,
+        udp: true,
+        unix: true,
+        raw: true
+      ),
+      'ss -a -m -n -t -u -x -w'
+    )
+
+    expect_command(host.ss(
+        family: 'inet'
+      ),
+      'ss -a -m -f inet'
+    )
+
+    expect {
+      host.ss(family: 'inet5')
+    }.to raise_error(ArgumentError)
+
+    expect_command(host.ss(
+        state: 'established'
+      ),
+      'ss -a -m state established'
+    )
+
+    expect_command(host.ss(
+        state: 'connected'
+      ),
+      'ss -a -m state connected'
+    )
+
+    expect {
+      host.ss(state: 'test')
+    }.to raise_error(ArgumentError)
+
+    expect_command(host.ss(
+        expression: "'( dport = :ssh or sport = :ssh )'"
+      ),
+      "ss -a -m '( dport = :ssh or sport = :ssh )'"
+    )
+  end
+end
